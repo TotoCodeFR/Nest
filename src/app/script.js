78c3032a1ab912bbbd02be4e8367f8c8634b41a3
TotoCode <totocodefr@duck.com>
2026-03-08 10:30:55 +0100

    open room in /

--- src/app/script.js
+++ src/app/script.js
@@ -4,7 +4,12 @@ async function loadRooms() {
     const res = await fetch("/api/rooms");
     const rooms = await res.json();
     rooms.forEach(room => {
-        const roomElement = roomTemplate.cloneNode(true);
+        const roomElement = roomTemplate.cloneNode(true).firstElementChild;
+
+        roomElement.addEventListener("click", (e) => {
+            window.location.href = `${room.name}`;
+        });
+        roomElement.querySelector(".roomName").textContent = room.name;
 
         document.getElementById("rooms").appendChild(roomElement);
     });
